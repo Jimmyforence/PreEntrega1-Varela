c3d4cef32f0e53a06de19796070db47b9261c49b
Jimmyforence <jimmyforence@gmail.com>
2022-11-02 02:45:47 -0600

    Starting my React Path
    I don't know what I am doing.

---
+++ store/src/componentes/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='navi'>NavBar
+        <h1 className='storeName'>Abuguet Store</h1>
+    <li>
+        <a href='' className='products'></a>
+    </li>
+    </div>
+  )
+}
+
+export default NavBar
